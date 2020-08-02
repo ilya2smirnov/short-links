8ce33e16f1f27a849431531bb5e457ec691b5939
smirnov_i <smirnov_i@protei.ru>
2020-08-02 20:45:34 +0300

    Fetch links after adding

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -1,17 +1,30 @@
 import axios from 'axios'
 
 async function addNewLinkAndGetCurrentList(fullLink) {
-  const data = {
+  const dataAdd = {
     user: 'max1',
     password: 'max3000',
     fullLink: fullLink,
   };
 
-  return axios.post('/api/short-links/add', data)
+  const dataGet = {
+    user: 'max1',
+    password: 'max3000',
+  };
+
+  return axios.post('/api/short-links/add', dataAdd)
     .then((res) => {
       console.log(`Link added: ${res.status}`);
       console.log('Add link resp: ', res.data);
+      return axios.post('/api/short-links/get', dataGet)
+        .then(res => {
+          console.log(`Get links: ${res.status}`);
+          console.log('Get links resp: ', res.data);
+          return res.data;
+        })
     }).catch((err) => {
     console.error(err);
   });
 }
+
+export default addNewLinkAndGetCurrentList;
